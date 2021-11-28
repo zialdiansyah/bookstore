825b6531cf454076dc8302f7a66b39ded451c6ef
zialdiansyah <ziaulhafizda@gmail.com>
2021-11-28 16:21:39 +0700

    styling with chakra-ui; added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import * as React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import { ChakraProvider, Flex, Box, Heading, Button, Spacer, Container } from '@chakra-ui/react'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Detail from './pages/Detail';
 import Library from './pages/Library';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to React!</h1>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/book/:id" element={<Detail />} />
-        <Route path="/library" element={<Library />} />
-        <Route path="/login" element={<Login />}/>
-      </Routes>
-    </div>
+    <ChakraProvider>
+      <Container maxWidth="800px">
+        <Flex py="2" mb={4}>
+          <Box p="2">
+            <Heading size="md"><Link to="/">BookTracker</Link></Heading>
+          </Box>
+          <Spacer />
+          <Box>
+            <Button colorScheme="teal" mr="4">
+              <Link to="/library">Library</Link>
+            </Button>
+            <Button colorScheme="teal" mr="4">
+              Logout
+            </Button>
+          </Box>
+        </Flex>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/library" element={<Library />} />
+          <Route path="/login" element={<Login />}/>
+        </Routes>
+      </Container>
+    </ChakraProvider>
   );
 }
 
